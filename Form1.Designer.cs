968aaa7ef784735a9365f189aa83b156f2c685b1
Hrs22 <34715351+Hrs22@users.noreply.github.com>
2022-02-16 19:17:49 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.title = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Lime;
+            this.title.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(60, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(469, 25);
+            this.title.TabIndex = 0;
+            this.title.Text = "Избери си категория мпс за изпитни въпроси";
+            this.title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 37);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(45, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "AM";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 58);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(40, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "A1";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            this.checkBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 83);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(40, 19);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "A2";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            this.checkBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 108);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(34, 19);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "A";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            this.checkBox4.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(12, 133);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(39, 19);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "B1";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            this.checkBox5.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(12, 158);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(33, 19);
+            this.checkBox6.TabIndex = 6;
+            this.checkBox6.Text = "B";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            this.checkBox6.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(12, 183);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(39, 19);
+            this.checkBox7.TabIndex = 7;
+            this.checkBox7.Text = "BE";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            this.checkBox7.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(12, 208);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(40, 19);
+            this.checkBox8.TabIndex = 8;
+            this.checkBox8.Text = "C1";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            this.checkBox8.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(12, 233);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(34, 19);
+            this.checkBox9.TabIndex = 9;
+            this.checkBox9.Text = "C";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            this.checkBox9.CheckedChanged += new System.EventHandler(this.checkBox9_CheckedChanged);
+            this.checkBox9.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(12, 254);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(34, 19);
+            this.checkBox10.TabIndex = 10;
+            this.checkBox10.Text = "D";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            this.checkBox10.CheckedChanged += new System.EventHandler(this.checkBox10_CheckedChanged);
+            this.checkBox10.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(12, 279);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(40, 19);
+            this.checkBox11.TabIndex = 11;
+            this.checkBox11.Text = "DE";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            this.checkBox11.CheckedChanged += new System.EventHandler(this.checkBox11_CheckedChanged);
+            this.checkBox11.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(12, 304);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(32, 19);
+            this.checkBox12.TabIndex = 12;
+            this.checkBox12.Text = "T";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            this.checkBox12.CheckedChanged += new System.EventHandler(this.checkBox12_CheckedChanged);
+            this.checkBox12.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox12_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(129, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 286);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(172, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Започване на изпит";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(589, 360);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox12);
+            this.Controls.Add(this.checkBox11);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Project";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label title;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox7;
+        private CheckBox checkBox8;
+        private CheckBox checkBox9;
+        private CheckBox checkBox10;
+        private CheckBox checkBox11;
+        private CheckBox checkBox12;
+        public PictureBox pictureBox1;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox11_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox11.ThreeState)
+            {
+                checkBox11.ThreeState = true;
+                checkBox11.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox11.ThreeState = false;
+                checkBox11.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+          
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            {
+                if (!checkBox1.ThreeState)
+                {
+                    checkBox1.ThreeState = true;
+                    checkBox1.CheckAlign = ContentAlignment.MiddleRight;
+                }
+                else
+                {
+                    checkBox1.ThreeState = false;
+                    checkBox1.CheckAlign = ContentAlignment.MiddleLeft;
+                }
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox2.ThreeState)
+            {
+                checkBox2.ThreeState = true;
+                checkBox2.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox2.ThreeState = false;
+                checkBox2.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox3.ThreeState)
+            {
+                checkBox3.ThreeState = true;
+                checkBox3.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox3.ThreeState = false;
+                checkBox3.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox4.ThreeState)
+            {
+                checkBox4.ThreeState = true;
+                checkBox4.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox4.ThreeState = false;
+                checkBox4.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox5.ThreeState)
+            {
+                checkBox5.ThreeState = true;
+                checkBox5.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox5.ThreeState = false;
+                checkBox5.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox6.ThreeState)
+            {
+                checkBox6.ThreeState = true;
+                checkBox6.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox6.ThreeState = false;
+                checkBox6.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox7.ThreeState)
+            {
+                checkBox7.ThreeState = true;
+                checkBox7.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox7.ThreeState = false;
+                checkBox7.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox8.ThreeState)
+            {
+                checkBox8.ThreeState = true;
+                checkBox8.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox8.ThreeState = false;
+                checkBox8.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox9_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox9.ThreeState)
+            {
+                checkBox9.ThreeState = true;
+                checkBox9.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox9.ThreeState = false;
+                checkBox9.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox10_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox10.ThreeState)
+            {
+                checkBox10.ThreeState = true;
+                checkBox10.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox10.ThreeState = false;
+                checkBox10.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox12_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox12.ThreeState)
+            {
+                checkBox12.ThreeState = true;
+                checkBox12.CheckAlign = ContentAlignment.MiddleRight;
+            }
+            else
+            {
+                checkBox12.ThreeState = false;
+                checkBox12.CheckAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void checkBox12_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false && checkBox2.Checked == false && checkBox3.Checked == false && checkBox4.Checked == false && checkBox5.Checked == false &&
+               checkBox6.Checked == false && checkBox7.Checked == false && checkBox8.Checked == false && checkBox9.Checked == false && checkBox10.Checked == false &&
+               checkBox11.Checked == false && checkBox12.Checked == false)
+            {
+                MessageBox.Show("Избери категория !");
+            }
+            if (checkBox1.Checked == true)
+            {
+                MessageBox.Show("Започваш категория АМ !");
+            }
+            else if (checkBox2.Checked == true)
+            {
+                MessageBox.Show("Започваш категория А1 !");
+            }
+            else if (checkBox3.Checked == true)
+            {
+                MessageBox.Show("Започваш категория А2 !");
+            }
+            else if (checkBox4.Checked == true)
+            {
+                MessageBox.Show("Започваш категория А !");
+            }
+            else if (checkBox5.Checked == true)
+            {
+                MessageBox.Show("Започваш категория B1 !");
+            }
+            else if (checkBox6.Checked == true)
+            {
+                MessageBox.Show("Започваш категория B !");
+            }
+            else if (checkBox7.Checked == true)
+            {
+                MessageBox.Show("Започваш категория BE !");
+            }
+            else if (checkBox8.Checked == true)
+            {
+                MessageBox.Show("Започваш категория C1 !");
+            }
+            else if (checkBox9.Checked == true)
+            {
+                MessageBox.Show("Започваш категория C !");
+            }
+            else if (checkBox10.Checked == true)
+            {
+                MessageBox.Show("Започваш категория D !");
+            }
+            else if (checkBox11.Checked == true)
+            {
+                MessageBox.Show("Започваш категория DE !");
+            }
+            else if (checkBox12.Checked == true)
+            {
+                MessageBox.Show("Започваш категория T !");
+            }
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
